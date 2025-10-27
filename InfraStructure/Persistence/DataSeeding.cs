fde7d59e8cf1433619ae8f1bfc02066221f3e610
EmanMohamed36 <ei3377898@gmail.com>
2025-10-28 01:41:39 +0300

    Demo: 1 => Apply(Onion Architecture , DB Models,DB Context ,Configurations,Migrations,Products-Brands-Types Data Seeding)

---
+++ InfraStructure/Persistence/DataSeeding.cs
@@ -0,0 +1,76 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using PersistenceLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PersistenceLayer
+{
+    public class DataSeeding(StoreDbContext _storeDbContext) : IDataSeeding
+    {
+        public void DataSeed()
+        {
+            try
+            {
+                if (_storeDbContext.Database.GetPendingMigrations().Any())
+                {
+                    _storeDbContext.Database.Migrate();
+                }
+
+                #region Add Product Brand Data
+
+                if (!_storeDbContext.ProductBrand.Any())
+                {
+                    var productBrandsData = File.ReadAllText(@"..\InfraStructure\Persistence\Data\DataSeed\brands.json");
+                    //Conver string to c# object
+                    var brand = JsonSerializer.Deserialize<List<ProductBrand>>(productBrandsData);
+                    if (brand is not null && brand.Any())
+                    {
+                        _storeDbContext.ProductBrand.AddRange(brand);
+                    }
+                }
+
+                #endregion
+
+                #region Add Product Types Data
+                if (!_storeDbContext.ProductTypes.Any())
+                {
+                    var productTypesData = File.ReadAllText(@"..\InfraStructure\Persistence\Data\DataSeed\types.json");
+                    //Conver string to c# object
+                    var type = JsonSerializer.Deserialize<List<ProductType>>(productTypesData);
+                    if (type is not null && type.Any())
+                    {
+                        _storeDbContext.ProductTypes.AddRange(type);
+                    }
+                }
+                #endregion
+
+                #region Add Product Data
+                if (!_storeDbContext.Products.Any())
+                {
+                    var productData = File.ReadAllText(@"..\InfraStructure\Persistence\Data\DataSeed\products.json");
+                    //Conver string to c# object
+                    var products = JsonSerializer.Deserialize<List<Product>>(productData);
+                    if (products is not null && products.Any())
+                    {
+                        _storeDbContext.Products.AddRange(products);
+                    }
+                }
+                #endregion
+
+                _storeDbContext.SaveChanges();
+
+            }
+            catch (Exception)
+            {
+
+                //ToDO
+            }
+        }
+    }
+}
